971dffba8013618096179b47adb5241c8d0c5425
Frederik Mellbye <frederik.mellbye@3lc.ai>
2024-08-13 13:12:11 +0200

    tmp add run files

--- classify.py
+++ classify.py
@@ -9,12 +9,17 @@ settings = Settings(
     sampling_weights=True,
     exclude_zero_weight_training=True,
     exclude_zero_weight_collection=True,
+    collection_epoch_start=0,
+    collection_epoch_interval=2,
+    run_description="My description",
+    run_name="My run name",
+    # image_embeddings_dim=3,
 )
 
 # Train the model using tables directly
 results = model.train(
     data="cifar10",
-    epochs=1,
+    epochs=10,
     imgsz=64,
     batch=64,
     settings=settings
--- classify_val_only.py
+++ classify_val_only.py
@@ -4,10 +4,14 @@ from ultralytics.utils.tlc import TLCYOLO, Settings
 
 model = TLCYOLO("yolov8n-cls.pt")
 
-settings = Settings()
+settings = Settings(
+    run_description="Collect mode for cifar10!",
+    image_embeddings_dim=2,
+)
 
-# data + split should be okay
-# table should be okay
-table = tlc.Table.from_url("/home/fredmell/yolov5-tutorial/3LC/cifar10-YOLOv8/datasets/cifar10-test/tables/original")
+tables = {
+    "train": tlc.Table.from_url("/home/fredmell/yolov5-tutorial/3LC/cifar10-YOLOv8/datasets/cifar10-train/tables/original"),
+    "test": tlc.Table.from_url("/home/fredmell/yolov5-tutorial/3LC/cifar10-YOLOv8/datasets/cifar10-test/tables/original"),
+}
 
-results = model.val(table=table, batch=32, imgsz=640, device=0, workers=0, settings=settings)
+results = model.collect(tables=tables, batch=32, imgsz=640, device=0, workers=0, settings=settings)
