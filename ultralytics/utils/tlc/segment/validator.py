e32ae771fe9cf2d60eb74c29f257a33aa64724de
Frederik Mellbye <frederik.mellbye@3lc.ai>
2025-02-10 13:11:04 +0100

    Cleanup

--- ultralytics/utils/tlc/segment/validator.py
+++ ultralytics/utils/tlc/segment/validator.py
@@ -1,14 +1,11 @@
 import numpy as np
 import tlc
 import torch
-import weakref
-import scipy
 
 from ultralytics.models.yolo.segment.val import SegmentationValidator
 from ultralytics.utils import ops
 # from ultralytics.utils.tlc.engine.validator import TLCValidatorMixin
 from ultralytics.utils.tlc.constants import IMAGE_COLUMN_NAME
-from ultralytics.utils.tlc.detect.utils import build_tlc_yolo_dataset
 from ultralytics.utils.tlc.detect.validator import TLCDetectionValidator
 from ultralytics.utils.tlc.segment.utils import compute_mask_iou, tlc_check_seg_dataset
 
@@ -26,8 +23,6 @@ class TLCSegmentationValidator(TLCDetectionValidator, SegmentationValidator):
         instance_properties_structure = {
             tlc.LABEL: tlc.CategoricalLabel(name=tlc.LABEL, classes=self.data["names"]),
             tlc.CONFIDENCE: tlc.Float(name=tlc.CONFIDENCE, number_role=tlc.NUMBER_ROLE_CONFIDENCE),
-            "matched_gt_iou": tlc.Float(name="matched_gt_iou", number_role=tlc.NUMBER_ROLE_IOU),
-            "matched_gt_id": tlc.Int(name="matched_gt_id", number_role="instance_index"),
         }
 
         segment_sample_type = tlc.InstanceSegmentationMasks(
@@ -61,10 +56,9 @@ class TLCSegmentationValidator(TLCDetectionValidator, SegmentationValidator):
                     "instance_properties": {
                         tlc.LABEL: [],
                         tlc.CONFIDENCE: [],
-                        "matched_gt_iou": [],
-                        "matched_gt_id": [],
                     },
-                    "masks": [],
+                    tlc.GT_IOUS: [],
+                    tlc.MASKS: [],
                 }
                 predicted_batch_segmentations.append(predicted_instances)
                 continue
@@ -83,39 +77,12 @@ class TLCSegmentationValidator(TLCDetectionValidator, SegmentationValidator):
             if len(cls) > 0:
                 mask_iou = compute_mask_iou(cls, pred_masks=pred_masks, gt_masks=gt_masks, overlap=True)
 
-                pred_class = pred_cls.cpu().numpy().astype(np.int32)
-                true_class = cls.cpu().numpy().astype(np.int32)
-                correct_class = true_class[:, None] == pred_class
-                iou = mask_iou.numpy() * correct_class
-
-                label_idx, prediction_idx = scipy.optimize.linear_sum_assignment(iou, maximize=True)
-                
-                # Initialize all predictions as unmatched (-1)
-                matched_gt_id = np.full(len(pred_cls), -1)
-                matched_gt_iou = np.zeros(len(pred_cls))
-                
-                # Only update the matched predictions
-                matched_gt_id[prediction_idx] = label_idx
-                matched_gt_iou[prediction_idx] = iou[label_idx, prediction_idx]
-
-                # Filter out matches with zero IoU first
-                valid = matched_gt_iou > 0
-                matched_gt_id[np.logical_not(valid)] = -1
-
-                # Map back to original instance order
+                # Reorder using instance mapping before transposing and converting to list
                 instance_mapping = batch["instance_mapping"][si]
-                reverse_instance_mapping = {v: i for i, v in instance_mapping.items()}
-                # Only map the valid matches (where matched_gt_id != -1)
-                matched_gt_id[valid] = np.array([reverse_instance_mapping[index] for index in matched_gt_id[valid]])
-
-                # TODO: Map back classes to original classes (see detection validator)
-
-                assert len(matched_gt_id) == len(pred_cls), "Number of matched ground truth ids does not match number of predictions"
-                # Assert matched_gt_ids greater than -1 are unique
-                assert len(np.unique(matched_gt_id[matched_gt_id > -1])) == len(matched_gt_id[matched_gt_id > -1]), "Matched ground truth ids are not unique"
+                indices = list(instance_mapping.keys())
+                mask_iou = mask_iou[indices, :].transpose(1, 0).tolist()
             else:
-                matched_gt_iou = torch.zeros(len(pred_cls))
-                matched_gt_id = torch.full_like(matched_gt_iou, -1)
+                mask_iou = [[] for _ in range(len(pred_cls))]
 
             # Scale masks to image size and handle padding
             pred_masks = torch.as_tensor(pred_masks, dtype=torch.uint8)
@@ -134,10 +101,9 @@ class TLCSegmentationValidator(TLCDetectionValidator, SegmentationValidator):
                 "instance_properties": {
                     tlc.LABEL: pred_cls.tolist(),
                     tlc.CONFIDENCE: conf.tolist(),
-                    "matched_gt_iou": matched_gt_iou.tolist(),
-                    "matched_gt_id": matched_gt_id.tolist(),
                 },
-                "masks": result_masks,
+                tlc.GT_IOUS: mask_iou, 
+                tlc.MASKS: result_masks,
             }
 
             predicted_batch_segmentations.append(predicted_instances)
